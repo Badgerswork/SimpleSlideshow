cff48f1aff23c43fdb1334b311b50d69d80e0cd9
Simon Thomas <simon@badgerworks.co.uk>
2019-03-26 10:13:15 +0000

    Iterate through multiple slide shows on a page
    Adding ability to handle multiple slideshows on one page

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
+// only scripts that do not rely on styling in here
+
 // Class Toggling Function
 // Pass in the element to toggle the class on and the class to toggle
 function toggleClass(elem, cls) {
-    elem.classList.toggle(cls);
+  elem.classList.toggle(cls);
 }
 
 // Slideshow function
 // Required the class of slide show on each of the images
 function slideshow() {
-    let thisEl = document.querySelectorAll('.slideshow-container__images .slideshow');
+  let slideGroup = document.querySelectorAll(".slideshow-container");
+  for (let index = 0; index < slideGroup.length; ++index) {
+    let thisEl = slideGroup[index].querySelectorAll(
+      ".slideshow-container .slideshow"
+    );
     let i = 1;
     let previous = 0;
     let current = 0;
 
+    setInterval(function() {
+      // We're using set interval as we want to keep looping over them
+      if (i === 0) {
+        previous = thisEl.length - 1; // Handle the switch back to the start (0 -1 wont exist) so zero base the number of elements
+      } else {
+        previous = i - 1;
+      }
+      current = i;
+      toggleClass(thisEl[current], "slideshow-visible");
+      toggleClass(thisEl[previous], "slideshow-visible");
 
-    setInterval(function() { // We're using set interval as we want to keep looping over them
-        if (i === 0) {
-            previous = thisEl.length - 1; // Handle the switch back to the start (0 -1 wont exist) so zero base the number of elements
-        } else {
-            previous = i - 1;
-        }
-        current = i;
-        toggleClass(thisEl[current], 'slideshow-visible');
-        toggleClass(thisEl[previous], 'slideshow-visible');
-
-        i = (i + 1) % thisEl.length; // reset the counter
+      i = (i + 1) % thisEl.length; // reset the counter
+      console.log(i);
     }, 5000);
+  }
 }
-
 document.addEventListener("DOMContentLoaded", function() {
-    slideshow();
- });
+  slideshow();
+});
